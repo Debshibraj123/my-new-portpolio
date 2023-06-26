af3d80353f35a78f54dfcc0a5ec9d92b33f6a04e
Shibraj <shibrajdeb456@gmail.com>
2023-06-26 11:54:11 +0530

    chnages done

--- src/content_option.js
+++ src/content_option.js
@@ -77,45 +77,55 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://plus.unsplash.com/premium_photo-1683983984371-1e8c32468699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZCUyMGRlbGl2ZXJ5fGVufDB8fDB8fHww&auto=format&fit=crop&w=1000&q=60",
+        description: "This is a user friendly responsive website made using reactjs",
+        link: "https://641c523e8b423f08ecb3aa1b--voluble-douhua-46fc91.netlify.app/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIU8dD4wdesuZVwYgY7AjHgQ2uMbUlOngIGwXIIX36TEN4yKs&s",
+        description: "This is the main Website of our College Society SAEINDIA NITDURGAPUR made using reactjs",
+        link: "https://www.saenitdgp.com/",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJ0DWSyZ_SJpqBRIw-9ziTA8-Zjf1JrTOXXM-fKqwEIHBr48&s",
+        description: "This is the main website of our College Techh Fest (Eastern India's Second largest) made using Django)",
+        link: "arhn.co.in",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1584697964190-7383cbee8277?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+        description: "Website made using Mern Stack where you can play quiz",
+        link: "https://github.com/Debshibraj123/quiz-app.git",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1668979324665-7218b59db345?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmV4dGpzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        description: "This is a Full stack website  Created using Nextjs",
+        link: "https://github.com/Debshibraj123/nextjs.git",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        description: "This website is created using reactjs and by fetching a third party api where you can read latest news",
+        link: "https://github.com/Debshibraj123/News_App.git",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+        description: "Simple crud application using django",
+        link: "https://github.com/Debshibraj123/crud-application-using-django.git",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://images.unsplash.com/photo-1535303311164-664fc9ec6532?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c29jaWFsJTIwbWVkaWF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Connect, share, and explore with our dynamic social media platform built on Django's powerful framework.",
+        link: "https://github.com/Debshibraj123/social-media-web-django.git",
+    },
+    {
+        img: "https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJsb2d8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Discover engaging articles, insightful opinions, and captivating stories on our Next.js-powered blog website. Join the conversation today!",
+        link: "https://github.com/Debshibraj123/blog-nextjs.git",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1494809610410-160faaed4de0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeSUyMG1hbmFnZW1lbnQlMjBzeXN0ZW18ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+        description: "Library Management website made using django",
+        link: "https://github.com/Debshibraj123/a4hackathon.git",
     },
     {
         img: "https://picsum.photos/400/?grayscale",
@@ -123,17 +133,7 @@ const dataportfolio = [{
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
@@ -152,7 +152,7 @@ const contactConfig = {
 
 const socialprofils = {
     github: "https://github.com/Debshibraj123",
-    facebook: "https://www.instagram.com/",
+    facebook: "https://www.instagram.com/_s_h_i_b_raj/",
     linkedin: "https://www.linkedin.com/in/shibraj-deb-26247b22a/",
     twitter: "https://twitter.com",
 };
